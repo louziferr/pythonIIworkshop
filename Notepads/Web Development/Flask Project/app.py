b9f27ce36c6b5724121b3dfc72864c04d392bd6a
luzieahrens <luzie.ahrens@gmail.com>
2023-09-12 13:34:19 +0200

    update app.py

--- Notepads/Web Development/Flask Project/app.py
+++ Notepads/Web Development/Flask Project/app.py
@@ -3,20 +3,24 @@ from flask import Flask, render_template
 # create app
 app = Flask(__name__)
 
-# read file contents
+# open file with data
 file = open('static/answers.csv')
-file.seek(0)
-answers = file.readlines()
-answers=[entry.strip("\n").split(",") for entry in answers]
 
+# read file
+lines = file.readlines()
+
+# reformat answers and save in list
+answers= [ entry.strip("\n").split(",") for entry in lines ]
+
+# route for showing results
 @app.route("/results")
 def results():
     return render_template('results.html', answers=answers)
 
+# route for start page
 @app.route("/")
 def index():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run()
